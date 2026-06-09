22667e7851ce0b3adf5dcbaca049c1ecf3db69ae
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-09 18:15:44 +0200

    added docstring to the coordinate class

--- A1_DT-Garden/Assets/Scripts/Grid/Coordinate.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/Coordinate.cs
@@ -7,6 +7,12 @@ namespace DefaultNamespace
         public Vector2 Position;
         public Section Section;
 
+        /// <summary>
+        /// The coordinate system for a tile. houses the position and what section of a tile it occupies
+        /// </summary>
+        /// <param name="x">The X position of the tile</param>
+        /// <param name="y">The Y position of the tile</param>
+        /// <param name="section">The section of the tile</param>
         public Coordinate(int x, int y, Section section)
         {
             this.Position = new(x, y);
