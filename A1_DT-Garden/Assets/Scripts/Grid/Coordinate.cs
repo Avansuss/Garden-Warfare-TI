c42d05ffc1e6d06b70f88a8e54d8df4f8d0c68c8
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-11 18:14:49 +0200

    coordinate: made all possible default constructors

--- A1_DT-Garden/Assets/Scripts/Grid/Coordinate.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/Coordinate.cs
@@ -15,17 +15,36 @@ namespace DefaultNamespace
             Position = new();
             Section = Section.Full;
         }
-        
+
+        /// <summary>
+        /// The coordinate system for a tile. houses the position and what section of a tile it occupies
+        /// </summary>
+        public Coordinate(int x, int y) : this(new(), Section.North) {}
+
+        /// <summary>
+        /// The coordinate system for a tile. houses the position and what section of a tile it occupies
+        /// </summary>
+        /// <param name="position">The XY Position of the tile</param>
+        public Coordinate(Vector2 position) : this(position, Section.North) {}
+
         /// <summary>
         /// The coordinate system for a tile. houses the position and what section of a tile it occupies
         /// </summary>
         /// <param name="x">The X position of the tile</param>
         /// <param name="y">The Y position of the tile</param>
         /// <param name="section">The section of the tile</param>
-        public Coordinate(int x, int y, Section section)
+        public Coordinate(int x, int y, Section section) : this(new Vector2(x, y), section) {}
+
+        /// <summary>
+        /// The coordinate system for a tile. houses the position and what section of a tile it occupies
+        /// </summary>
+        /// <param name="position">The XY Position of the tile</param>
+        /// <param name="section">The section of the tile</param>
+        public Coordinate(Vector2 position, Section section)
         {
-            this.Position = new(x, y);
+            this.Position = position;
             this.Section = section; 
         }
+
     }
 }
