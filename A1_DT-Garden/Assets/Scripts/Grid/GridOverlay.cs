241214b032c6b0774ce52b1b7f149ffb42d8297d
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-11 11:24:13 +0200

    gridoverlay: forgot to change size to vector2int

--- A1_DT-Garden/Assets/Scripts/Grid/GridOverlay.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/GridOverlay.cs
@@ -11,7 +11,7 @@ namespace DefaultNamespace
     {
         public GridManager GridManager;
 
-        private Vector2 size;
+        private Vector2Int size;
 
         private void Start()
         {
