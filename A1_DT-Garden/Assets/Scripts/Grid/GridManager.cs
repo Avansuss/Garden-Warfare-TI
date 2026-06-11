fd26fad2411f714780b55f9cb6bbb7e7241fb284
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-11 18:22:55 +0200

    test: added a test object to the scene for grid snapping testing

--- A1_DT-Garden/Assets/Scripts/Grid/GridManager.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/GridManager.cs
@@ -16,7 +16,7 @@ public class GridManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public bool SetTile(Coordinate placement, GridTileType type)
