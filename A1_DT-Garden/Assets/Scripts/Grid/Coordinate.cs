8e818a43c36fee389dd37a1903008730a57a954e
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-11 18:03:41 +0200

    coordinate: added an empty default constructor

--- A1_DT-Garden/Assets/Scripts/Grid/Coordinate.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/Coordinate.cs
@@ -10,6 +10,15 @@ namespace DefaultNamespace
         /// <summary>
         /// The coordinate system for a tile. houses the position and what section of a tile it occupies
         /// </summary>
+        public Coordinate()
+        {
+            Position = new();
+            Section = Section.Full;
+        }
+        
+        /// <summary>
+        /// The coordinate system for a tile. houses the position and what section of a tile it occupies
+        /// </summary>
         /// <param name="x">The X position of the tile</param>
         /// <param name="y">The Y position of the tile</param>
         /// <param name="section">The section of the tile</param>
