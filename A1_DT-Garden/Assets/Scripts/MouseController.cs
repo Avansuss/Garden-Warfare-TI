8f73e03bf9c329838ee0161256bdd2ed96067591
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-11 18:24:36 +0200

    mousecontroller: moved a restriction condition to the proper place

--- A1_DT-Garden/Assets/Scripts/MouseController.cs
+++ A1_DT-Garden/Assets/Scripts/MouseController.cs
@@ -24,48 +24,49 @@ public class MouseController : MonoBehaviour
     {
         var coordinate = GetGridSnappedMousePos();
         
-        testObj.transform.eulerAngles = new Vector3(0, coordinate.GetAngle() + 90, 0);
-        testObj.transform.position = coordinate.Position;
+        // Don't update the position outside of the bounds
+        if (coordinate.Position.x > 0 && coordinate.Position.x < manager.Size.x &&
+            coordinate.Position.y > 0 && coordinate.Position.y < manager.Size.y)
+        {
+
+            testObj.transform.eulerAngles = new Vector3(0, coordinate.GetAngle() + 90, 0);
+            testObj.transform.position = coordinate.Position;
+        }
     }
 
     private Coordinate GetGridSnappedMousePos(bool getFullTile = false)
     {
         var cursorPos = cam.ScreenToWorldPoint(mouse.position.ReadValue());
-
-        // Don't update the position outside of the bounds
-        if (cursorPos.x > 0 && cursorPos.x < manager.Size.x && cursorPos.z > 0 && cursorPos.z < manager.Size.y)
-        {
+        
             cursorPos.y = 0;
 
-            // Get the decimals of the cursor position, centered aroudn the middle
-            var decX = cursorPos.x % 1 - 0.5f;
-            var decXAbs = math.abs(decX);
-            var decZ = cursorPos.z % 1 - 0.5f;
-            var decZAbs = math.abs(decZ);
+        // Get the decimals of the cursor position, centered aroudn the middle
+        var decX = cursorPos.x % 1 - 0.5f;
+        var decXAbs = math.abs(decX);
+        var decZ = cursorPos.z % 1 - 0.5f;
+        var decZAbs = math.abs(decZ);
 
-            // Snap to middle of grid
-            cursorPos.x = math.floor(cursorPos.x) + 0.5f;
-            cursorPos.z = math.floor(cursorPos.z) + 0.5f;
+        // Snap to middle of grid
+        cursorPos.x = math.floor(cursorPos.x) + 0.5f;
+        cursorPos.z = math.floor(cursorPos.z) + 0.5f;
 
-            // Calculate what triangle the mouse is in
-            Coordinate coordinate = new(cursorPos);
-            if (getFullTile)
+        // Calculate what triangle the mouse is in
+        Coordinate coordinate = new(cursorPos);
+        if (getFullTile)
+        {
+            coordinate.Section = Section.Full;
+        }
+        else
+        {
+            if (decZAbs > decXAbs)
             {
-                coordinate.Section = Section.Full;
+                coordinate.Section = decZ < 0 ? Section.North : Section.South;
             }
             else
             {
-                if (decZAbs > decXAbs)
-                {
-                    coordinate.Section = decZ < 0 ? Section.North : Section.South;
-                }
-                else
-                {
-                    coordinate.Section = decX < 0 ? Section.West : Section.East; 
-                }
+                coordinate.Section = decX < 0 ? Section.West : Section.East; 
             }
-
-            return coordinate;
         }
+        return coordinate;  
     }
 }
