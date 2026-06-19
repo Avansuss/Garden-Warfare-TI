22728d99307981b11514ecb269cec0a9119064ca
JarmoMarczinkowsky <jeh.marczinkowsky@student.avans.nl>
2026-06-19 11:51:43 +0200

    Added feedback on selected tile type

--- A1_DT-Garden/Assets/Scripts/UI/UIManager.cs
+++ A1_DT-Garden/Assets/Scripts/UI/UIManager.cs
@@ -1,16 +1,31 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //references to the UI elements
+    //https://www.flaticon.com/free-icon/paint-bucket_483918
+    //https://www.flaticon.com/free-icon/download_724933
+
+    public List<Button> drawingTypeButtons;
+    public int chosenId = 0;
+
+    public void SelectDrawingType()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SelectTileType(int btnTextureID)
     {
-        
+        switch(btnTextureID)
+        {
+            case 2:
+                chosenId = btnTextureID;
+                Debug.Log("Selected grass tile type");
+                break;
+
+        }
     }
 }
