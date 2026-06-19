a10190b716a45e89454e7a5329f75a755bd93b8a
Pr0xximity999 <tomsomgamer999@gmail.com>
2026-06-19 11:34:43 +0200

    grid agent: added the scaffolding for the agent

---
+++ A1_DT-Garden/Assets/Scripts/GridEditorAgent.cs
@@ -0,0 +1,28 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class GridEditorAgent : Agent
+{
+    public GridManager manager;
+    
+    private void Start()
+    {
+
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+
+    }
+}
