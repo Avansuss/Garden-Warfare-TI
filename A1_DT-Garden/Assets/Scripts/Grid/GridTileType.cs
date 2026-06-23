afb0b9162327a337d9f52a13d03da90f28a5b882
JarmoMarczinkowsky <jeh.marczinkowsky@student.avans.nl>
2026-06-23 15:43:19 +0200

    Expanded gridtiletypes to contain everything

--- A1_DT-Garden/Assets/Scripts/Grid/GridTileType.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/GridTileType.cs
@@ -5,7 +5,26 @@ namespace Grid
         Inactive = 0,
         Empty = 1,
         Grass = 2,
-        Rock = 3,
-        Water = 4,
+        Pond = 3,
+        SwimmingPool = 4,
+        Tiles = 5,
+        PermeableTiles = 6,
+        Gravel = 7,
+        RootFabric = 8,
+        ArtificialGrass = 9,
+        Trampoline = 10,
+        Playground = 11,
+        PlaygroundSand = 13,
+        Flowers = 14,
+        WoodChips = 15,
+        VegetableGarden = 16,
+        Hedge = 17,
+        Bush = 18,
+        PluckingGarden = 19,
+        Tree = 20
+
+        //Rock = 3,
+        //Water = 4,
+
     }
 }
