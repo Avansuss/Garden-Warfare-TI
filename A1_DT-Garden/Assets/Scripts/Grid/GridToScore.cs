82bd459d5ad076bf03dd0a6c37cec26b94841cf2
JarmoMarczinkowsky <jeh.marczinkowsky@student.avans.nl>
2026-06-26 14:38:18 +0200

    Added call to visualizer method

--- A1_DT-Garden/Assets/Scripts/Grid/GridToScore.cs
+++ A1_DT-Garden/Assets/Scripts/Grid/GridToScore.cs
@@ -26,17 +26,25 @@ public class GridToScore
         setSoilHandlingValues(answersDropdown);
         setScores(drawnTiles);
 
-        Debug.Log($"Amount of ponds: {scoreData.AreaPond}");
 
         scoreCalculate = new ScoreCalculation(scoreData);
         scoreModifier = new ScoreModifier(scoreCalculate);
         scoreManager = new ScoreManager(scoreData, scoreCalculate, scoreModifier, gardenAnimalsData);
 
-        Debug.Log($"Soil Water: {scoreManager.SoilWater()}");
-        Debug.Log($"Healthy Soil: {scoreManager.HealthySoil(fertilizerType, greenWasteLeftInGarden)}"); //fix amount of fertilizer and green waste in garden
-        Debug.Log($"Life Above The Soil: {scoreManager.LifeAboveTheSoil()}");
-        Debug.Log($"Plant Diversity: {scoreManager.PlantDiversity(plantAmount)}"); //fix amount of plants in garden
-        Debug.Log("Plant Amount: " + plantAmount);
+        float soilWaterValue = scoreManager.SoilWater();
+        float soilHealth = scoreManager.HealthySoil(fertilizerType, greenWasteLeftInGarden);
+        float animalFriendliness = scoreManager.LifeAboveTheSoil();
+        float plantDiversity = scoreManager.PlantDiversity(plantAmount);
+
+        Debug.Log($"Soil Water Value: {soilWaterValue}");
+        Debug.Log($"Soil Health: {soilHealth}");
+        Debug.Log($"Animal Friendliness: {animalFriendliness}");
+        Debug.Log($"Plant Diversity: {plantDiversity}");
+
+        float[] calculatedPillars = new float[4] { soilWaterValue, soilHealth, animalFriendliness, plantDiversity } ;
+
+        scoreVisualizer.VisualizeScore(calculatedPillars);
+
     }
 
     private void setScores(Dictionary<Coordinate, GridTile> drawnTiles)
