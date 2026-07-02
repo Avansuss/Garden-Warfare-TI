92e9285b0773b71b7b3bd7d70caa2c846419760a
JarmoMarczinkowsky <jeh.marczinkowsky@student.avans.nl>
2026-07-02 18:46:20 +0200

    Added method for changing view

--- A1_DT-Garden/Assets/Scripts/UI/UIManager.cs
+++ A1_DT-Garden/Assets/Scripts/UI/UIManager.cs
@@ -30,6 +30,9 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Toggle[] questionsToggle;
     [SerializeField] private TMP_Dropdown[] questionsDropdowns;
     [SerializeField] private TMP_Text lblExplanation;
+    [SerializeField] private Camera topDownCamera;
+    [SerializeField] private Camera threeDimensionalCamera;
+    [SerializeField] private GameObject pnlSecondCamera;
 
     private bool[] questionsAnimals = new bool[4];
     private byte[] answersDropdown = new byte[2];
@@ -141,4 +144,11 @@ public class UIManager : MonoBehaviour
     {
         lblExplanation.text = "";
     }
+
+    public void SwitchActiveCamera()
+    {
+        pnlSecondCamera.gameObject.SetActive(!pnlSecondCamera.gameObject.activeSelf);
+    }
+
+
 }
